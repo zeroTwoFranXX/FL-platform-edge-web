f4f9215f9b1ac34aad383a6c0381c684a6d8d320
zhenghe <2430897416@qq.com>
2023-04-05 19:34:15 +0800

    first commit

---
+++ src/public/configs/config.js
@@ -0,0 +1,22 @@
+/**
+ * 配置规则：
+ * （1）'$'符号开头的合法变量名
+ * （2）配置环境变量固定变量名为：$env
+ * （3）如果变量的值为null,undefined,''或0，则不会进行属性的添加（覆盖）
+ */
+// $env = ''
+// $baseUrl = ''
+
+//
+// export const BASE_URL = 'http://192.168.179.185:14558'
+// export const BASE_URL = 'http://8.140.135.129:14558'
+// export const BASE_URL = 'http://flp.free.svipss.top'// 集成内网穿透API
+// export const BASE_URL = 'http://10.28.182.57:8989' // 冯煜内网API
+// export const BASE_URL = 'http://flp.free.svipss.top' // 郑州内网穿透API
+// export const USER_URL = 'http://192.168.179.185:8000' // 用户中心
+// export const MODEL_URL = 'http://8.140.135.129:14559' // 模型中心
+// export const PROJECT_URL = 'http://8.140.135.129:9090' // 项目管理
+export const BASE_URL = 'http://10.112.216.104:14556'
+// export const BASE_URL = 'http://10.112.169.70:14558'
+// export const BASE_URL = 'http://10.38.29.196:8877'
+
